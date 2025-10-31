15e4e59fdbbc1afe778dcb1a50bf1d50f4f0e81a
IshantRaut <ishjee2018@gmail.com>
2025-10-31 09:10:48 +0530

    Four SUm Brute and optimal approach

---
+++ Arrays/4Sum/brute.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+vector<vector<int>> fourSum(vector<int>& nums,int target){
+int n=nums.size();
+set<vector<int>> st;
+
+for(int i =0;i<n-1;i++){
+    for(int j=i+1;j<n-1;j++){
+        for(int k=j+1;k<n-1;k++){
+            for(int l=k+1;l<n-1;l++){
+                if(nums[i]+nums[j]+nums[k]+nums[l]==target){
+                    vector<int> temp={nums[i],nums[j],nums[k],nums[l]};
+                    sort(temp.begin(),temp.end());
+                    st.insert(temp);
+                }
+            }
+        }
+    }
+     vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+}
+}
+int main()
+{
+    vector<int> nums = {4, 3, 3, 4, 4, 2, 1, 2, 1, 1};
+    int target = 9;
+    vector<vector<int>> ans = fourSum(nums, target);
+    cout << "The quadruplets are: \n";
+    for (auto it : ans) {
+        cout << "[";
+        for (auto ele : it) {
+            cout << ele << " ";
+        }
+        cout << "] ";
+    }
+    cout << "\n";
+    return 0;
+}
