c1401d0bbc6e81b34d06c59b93a5214739190c7e
IshantRaut <ishjee2018@gmail.com>
2025-11-08 16:04:30 +0530

    Smallest Divisor

---
+++ BnarySearch/KokoEatingBananas/koko.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main() {
+    vector<int> piles = {3, 6, 7, 11};
+    int h = 8;
+
+    Solution obj;
+    cout << obj.minEatingSpeed(piles, h);
+    return 0;
+}
